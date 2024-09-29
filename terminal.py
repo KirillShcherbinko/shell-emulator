1ff22bf4dafafd453ca4ecfd424017aa98cdc66e
Kirill <kirill.sherbinko@gmail.com>
2024-09-29 18:43:42 +0300

    added ls and cd functions, fixed mv function, updated pwd function

--- terminal.py
+++ terminal.py
@@ -28,15 +28,36 @@ class Terminal(object):
             self.vfsFiles = vfs_file.namelist()
         vfs_file.close()
         for i in range(len(self.vfsFiles)):
-            top_file = self.vfsFiles[i].split('/')[0]
-            if top_file not in self.vfsFiles:
-                self.vfsFiles.append(top_file)
             self.vfsFiles[i] = self.vfsFiles[i].rstrip('/')
+            top_files = self.vfsFiles[i].split('/')
+            top_file = ""
+            for j in range(len(top_files)):
+                top_file += top_files[j] + '/'
+                if top_file.rstrip('/') not in self.vfsFiles:
+                    self.vfsFiles.append(top_file.rstrip('/'))
+
+    def replace_basename(self, file_path, old, new):
+        return file_path[::-1].replace(old[::-1], new[::-1], 1)[::-1]
+
+    def get_previous_dir(self, file_path_parts, cur_dir):
+        if cur_dir == "" or not ".." in file_path_parts:
+            return cur_dir
+        file_basename = path.basename(cur_dir)
+        cur_dir = self.replace_basename(cur_dir, file_basename, "").rstrip('/')
+        return self.get_previous_dir(file_path_parts[1:], cur_dir)
 
     def get_full_path(self, file_path):
-        if self.cur_dir in file_path:
-            return file_path
-        return self.cur_dir + file_path
+        if file_path == "~":
+            return ""
+        if file_path == ".":
+            return self.cur_dir
+        if ".." in file_path:
+            file_path_parts = file_path.rstrip('/').split('/')
+            real_file_path_parts = file_path.rstrip('/').split('.')
+            return (self.get_previous_dir(file_path_parts, self.cur_dir) + '/' + real_file_path_parts[-1]).rstrip('/')
+        if file_path[:2] == "~/" or self.cur_dir == "":
+            return file_path.lstrip("~/")
+        return self.cur_dir + '/' + file_path
 
     def rename_check(self, old_file_name, new_file_name):
         old_file_extension = get_extension(old_file_name)
@@ -51,20 +72,18 @@ class Terminal(object):
         old_file_name = path.basename(self.old_file_path)
         new_file_name = path.basename(self.new_file_path)
         if self.old_file_path not in self.vfsFiles:
-            print(f'Файла {self.old_file_path} не существует')
+            print("Файл не найден")
             return False
-        if self.new_file_path == "~":
-            self.new_file_path = ""
-        elif self.new_file_path not in self.vfsFiles:
+        if self.new_file_path not in self.vfsFiles and self.new_file_path != "":
             if self.rename_check(old_file_name, new_file_name):
-                real_new_file_path = self.new_file_path.replace('/' + new_file_name, '')
+                real_new_file_path = self.replace_basename(self.new_file_path, new_file_name, '').rstrip('/')
                 if real_new_file_path not in self.vfsFiles and new_file_name != self.new_file_path:
-                    print(f'Директории {self.new_file_path} не существует')
+                    print("Файл не найден")
                     return False
                 return True
             print(f'Недопустимое имя файла {self.new_file_path}')
             return False
-        self.new_file_path = path.join(self.new_file_path, old_file_name)
+        self.new_file_path = (self.new_file_path + '/' +  old_file_name).strip('/')
         return True
 
     def move_file(self):
@@ -77,6 +96,12 @@ class Terminal(object):
                         tmp_vfs_file.writestr(new_file, vfs_file.read(file))
                     else:
                         tmp_vfs_file.writestr(file, vfs_file.read(file))
+            for empty_folder in self.vfsFiles:
+                if (self.new_file_path in empty_folder
+                        and get_extension(empty_folder) == empty_folder
+                        and empty_folder + '/' not in vfs_file.namelist()
+                        and empty_folder + '/' not in tmp_vfs_file.namelist()):
+                    tmp_vfs_file.writestr(empty_folder + '/', '')
         remove(self.vfsPath)
         move(tmp_vfs_path, self.vfsPath)
 
@@ -88,7 +113,7 @@ class Terminal(object):
 
     def command_selector(self, message_data):
         if len(message_data) == 0:
-            print()
+            return
         elif message_data[0] == "ls":
             self.ls(message_data[1:])
         elif message_data[0] == "cd":
@@ -106,22 +131,45 @@ class Terminal(object):
 
     def ls(self, message_data):
         try:
+            if len(message_data) > 1:
+                print("Введено слишком много аргументов")
+                return
+            if len(message_data) == 0:
+                message_data.append('.')
+            dir_name = self.get_full_path(message_data[0])
+            for file in self.vfsFiles:
+                file_basename = path.basename(file)
+                if dir_name == self.replace_basename(file, file_basename, "").rstrip('/'):
+                    print(file_basename, end = '    ')
             print()
         except:
-            print()
+            print("Не удалось выполнить команду")
 
     def cd(self, message_data):
-        print()
+        try:
+            if len(message_data) == 0:
+                self.cur_dir = ""
+                return
+            elif len(message_data) > 1:
+                print("Слишком много аргументов для функции cd")
+                return
+            new_dir = self.get_full_path(message_data[0].rstrip('/'))
+            if new_dir in self.vfsFiles and get_extension(new_dir) == new_dir:
+                self.cur_dir = new_dir
+                return
+            if new_dir == "":
+                return
+            print("Не удалось найти директорию с таким именем")
+        except:
+            print("Не удалось выполнить команду")
+
 
     def exit(self):
         print('exit')
         self.processing = False
 
     def pwd(self):
-        if self.cur_dir == '':
-            print("~")
-            return
-        print(self.cur_dir)
+        print(("/home/" + self.username + '/' + self.cur_dir).rstrip('/'))
 
     def whoami(self, message_data):
         if len(message_data) > 0:
@@ -131,15 +179,18 @@ class Terminal(object):
 
     def mv(self, message_data):
         try:
+            if len(message_data) > 2:
+                print("Введено слишком много аргументов")
+                return
             if len(message_data) < 2:
                 print("Введено не достаточно аргументов")
                 return
-            self.old_file_path = self.get_full_path(message_data[0])
-            self.new_file_path = self.get_full_path(message_data[1])
+            self.old_file_path = self.get_full_path(message_data[0].rstrip('/'))
+            self.new_file_path = self.get_full_path(message_data[1].rstrip('/'))
             if self.move_ability():
                 self.move_file()
                 self.get_files_list(self.vfsPath)
-                self.old_file_path = ""
-                self.new_file_path = ""
+            self.old_file_path = ""
+            self.new_file_path = ""
         except:
             print("Не удалось выполнить команду")
